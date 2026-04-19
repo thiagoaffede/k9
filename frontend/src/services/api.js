4efa10708475fa5684d482ec739c92b725093c4c
Thiago Affede <62530514+thiagoaffede@users.noreply.github.com>
2026-04-19 11:32:45 +0100

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://localhost:5000/api',
-  baseURL: "https://double-trace-nerve-seasons.trycloudflare.com/api",
+  baseURL: "https://usr-geneva-absorption-casual.trycloudflare.com/api",
 });
 
 api.interceptors.request.use((config) => {
